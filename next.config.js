22edbca7fcb1c77102a1f3326c312f233f4307d0
SharkEzz <icraft640@gmail.com>
2022-08-13 15:13:08 +0200

    Revert

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXT_PUBLIC_DIRECTUS_URL: process.env.NEXT_PUBLIC_DIRECTUS_URL,
-  }
 }
 
 module.exports = nextConfig
