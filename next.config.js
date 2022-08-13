9ee26497053bd4773efeffb60a56b4cba0a72489
SharkEzz <icraft640@gmail.com>
2022-08-13 15:10:22 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_DIRECTUS_URL: process.env.NEXT_PUBLIC_DIRECTUS_URL,
+  }
 }
 
 module.exports = nextConfig
