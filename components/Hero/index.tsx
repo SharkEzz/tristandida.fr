336a06b4db22ab4a2f494644681bde74ed6cba30
SharkEzz <icraft640@gmail.com>
2022-09-25 12:52:46 +0200

    Responsive fix

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,7 +2,15 @@ import { Container, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 
 export default function Hero({ title, subtitle }: { title: string; subtitle: string }) {
   return (
-    <Flex as="header" minH="400px" bgColor="blue.900" align="center" color="white" p={6}>
+    <Flex
+      as="header"
+      minH="400px"
+      bgColor="blue.900"
+      align="center"
+      color="white"
+      p={6}
+      pt={['72px', null, null, 6]}
+    >
       <Container maxW="container.lg">
         <VStack spacing={6}>
           <Heading fontSize="5xl" as="h1" color="white" textAlign="center">
