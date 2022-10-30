d5e65b3783bf1ca3b6284c5be18eb3ea8df053e5
SharkEzz <icraft640@gmail.com>
2022-10-30 11:52:30 +0100

    fix again :(

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   output: 'standalone',
   compiler: {
     emotion: true,
-  }
+  },
+  env: {
+    NEXT_PUBLIC_APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION
+  },
 }
 
 module.exports = nextConfig
